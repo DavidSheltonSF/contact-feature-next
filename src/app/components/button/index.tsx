622509e9a52d907878e14033d9e9653df931eb3c
DavidSheltonSF <david.faria012@gmail.com>
2025-09-26 14:47:30 -0300

    feat(button): add transition and color changing on hover

--- src/app/components/button/index.tsx
+++ src/app/components/button/index.tsx
@@ -4,7 +4,7 @@ interface ButtonProps {
 
 export function Button({label}: ButtonProps) {
   return (
-    <button className="bg-blue-500 text-white font-bold rounded-md py-1 cursor-pointer" type="submit">
+    <button className="bg-blue-500 text-white font-bold rounded-md py-1 cursor-pointer hover:bg-sky-500 transition delay-5 ease-linear" type="submit">
       {label}
     </button>
   )
