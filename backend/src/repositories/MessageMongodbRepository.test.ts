27ee87e06891cb0d59dab06185b9f5cc215908fb
DavidSheltonSF <david.faria012@gmail.com>
2025-09-26 18:49:28 -0300

    refactor(MessageMongodbRepository)

---
+++ backend/src/repositories/MessageMongodbRepository.test.ts
@@ -0,0 +1,46 @@
+import { MessageMongoDbRepository } from './MessageMongodbRepository';
+import { MongodbHelper } from './helpers/MongodbHelper';
+import dotenv from 'dotenv'
+dotenv.config()
+
+describe('Testing MongodbHelper', () => {
+  const mongoHelper = MongodbHelper.getInstance()
+
+  beforeAll(async () => {
+    const uri = process.env.MONGO_URI;
+    if (!uri){
+      throw Error('Mongodb URI was not found')
+    }
+    await mongoHelper.connect(uri)
+  });
+
+  beforeEach(async () => {
+    await mongoHelper.clearCollection('messages');
+  })
+
+  afterAll(async () => {
+   await  mongoHelper.disconnect()
+  })
+
+  test('Should create a new message', async () => {
+    const mongoHelper = MongodbHelper.getInstance()
+    const messagesCollection = mongoHelper.getCollection('messages');
+    const messageRepository = new MessageMongoDbRepository()
+
+    const message = {
+      username: 'Jeraldo',
+      email: 'jera@email.com',
+      text: "This is a simple message",
+      createdAt: new Date().toString()
+    }
+
+    const result = await messageRepository.create(message);
+
+    const createdMessage = await messagesCollection.findOne({_id: result.insertedId})
+    
+    expect(createdMessage?.username).toBe(message.username);
+    expect(createdMessage?.email).toBe(message.email);
+    expect(createdMessage?.text).toBe(message.text);
+  });
+
+})
---
+++ backend/src/repositories/MessageMongodbRepository.ts
@@ -0,0 +1,30 @@
+import { MongodbHelper } from "./helpers/MongodbHelper"
+import { MessageProps, MessageModel } from "../types/message";
+
+const mongoHelper = MongodbHelper.getInstance()
+
+export class MessageMongoDbRepository {
+
+  messsageCollection = mongoHelper.getCollection('messages')
+
+  async create(message: MessageProps) {
+    const result = await this.messsageCollection.insertOne({...message, createdAt: new Date()});
+    return result;
+  }
+
+  async findAll(): Promise<MessageModel[]> {
+    const result = await this.messsageCollection.find().toArray();
+
+    const mappedResults = result.map(msg => {
+      return {
+        _id: msg._id,
+        username: msg.username,
+        email: msg.email,
+        text: msg.text,
+        createdAt: msg.createdAt
+      }
+    });
+
+    return mappedResults;
+  }
+}
