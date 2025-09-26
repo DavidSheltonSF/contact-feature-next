4d38347a212bb5709ce432ed980dde7980b0f7ba
DavidSheltonSF <david.faria012@gmail.com>
2025-09-26 10:21:43 -0300

    set up

---
+++ src/app/components/navbar/index.tsx
@@ -0,0 +1,14 @@
+export function NavBar() {
+  return (
+    <header>
+      <nav className="bg-blue-">
+        <div>
+          <img src="chat.png" alt="" />
+        </div>
+        <span>
+          Live Demo
+        </span>
+      </nav>
+    </header>
+  )
+}
