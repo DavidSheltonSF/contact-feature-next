9f7abcdf4c319e526ef6f2064245f8f7e79c4b80
DavidSheltonSF <david.faria012@gmail.com>
2025-09-26 14:22:55 -0300

    feat(components): create MessageItem component

---
+++ src/app/components/messageItem/index.tsx
@@ -0,0 +1,35 @@
+interface MessageProps {
+  username: string;
+  email: string;
+  body: string;
+  // createdAt: string
+}
+
+export function MessageItem(props: MessageProps) {
+
+  const {username, email, body} = props
+ 
+  return (
+    <article className="flex flex-col gap-4 h-30">
+    <header className="flex gap-3">
+      <div className="flex">
+        <span className="bg-blue-500 px-3 py-4 rounded-xl">Icon</span>
+      </div>
+      <div className="flex flex-col justify-center mr-auto">
+        <h3 className="font-bold">{username}</h3>
+        <div className="flex items center gap-1">
+          <img className="opacity-50" src="email-icon.svg" alt="" /> <span className="text-gray-500">{email}</span>
+        </div>
+      </div>
+      <div>
+          <div className="flex gap-1">
+          <img className="opacity-25" src="clock-icon.svg" alt="" /> <span className="text-gray-400">less than a minute ago</span>
+        </div>
+      </div>
+    </header>
+    <main>
+      <p>{body}</p>
+    </main>
+  </article>
+  )
+}
