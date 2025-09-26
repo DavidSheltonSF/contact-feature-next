f2e167479d62ddb7c6bcae9e1a6de7fcd5985018
DavidSheltonSF <david.faria012@gmail.com>
2025-09-26 16:57:19 -0300

    feat(fakeDatabase): add fakeDatabase for tests

---
+++ backend/src/database/fakeDatabase.ts
@@ -0,0 +1,14 @@
+export const fakeMessages = [
+  {
+    name: 'Joseth',
+    email: "jo@email.com",
+    message: "This is a test message",
+    createdAt: "2020-01-22"
+  },
+  {
+    name: 'Mario',
+    email: "jo@email.com",
+    message: "This is a test message",
+    createdAt: "2025-02-02"
+  },
+]
