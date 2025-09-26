3ab852778b9005dbde21484ebdb9eb2edb6c9deb
DavidSheltonSF <david.faria012@gmail.com>
2025-09-26 14:23:15 -0300

    feat(components): create NoMessage component

---
+++ src/app/components/noMessage/index.tsx
@@ -0,0 +1,8 @@
+export function NoMessage() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h2 className="font-bold">No messages yet</h2>
+      <p>Be the first to send us a message</p>
+    </div>
+  )
+}
