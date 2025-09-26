e8459d43af31216060d793c63426500561901750
DavidSheltonSF <david.faria012@gmail.com>
2025-09-26 10:37:05 -0300

    feat(navbar) added icon and 'Live Demo' span

--- src/app/components/navbar/index.tsx
+++ src/app/components/navbar/index.tsx
@@ -1,11 +1,11 @@
 export function NavBar() {
   return (
     <header>
-      <nav className="bg-blue-">
-        <div>
+      <nav className="flex p-5 bg-gray-50 drop-shadow-sm">
+        <div className="mr-auto">
           <img src="chat.png" alt="" />
         </div>
-        <span>
+        <span className="bg-green-200 py-1 px-2 rounded-2xl font-bold">
           Live Demo
         </span>
       </nav>
