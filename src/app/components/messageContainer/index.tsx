2a2a5aade1a9a54a9e12458e177763843ab1ace6
DavidSheltonSF <david.faria012@gmail.com>
2025-09-27 08:33:33 -0300

    refactor(MessageContainer): make element a client one

--- src/app/components/messageContainer/index.tsx
+++ src/app/components/messageContainer/index.tsx
@@ -1,20 +1,35 @@
+'use client'
+
 import { NoMessage } from "../noMessage"
 import { Messages } from "../messages"
+import { useEffect, useState } from "react";
 
-export async function MessageContainer() {
+export function MessageContainer({formState}: {formState: any}) {
 
-    const response = await fetch('http://localhost:8080/messages', {
-      method: 'GET'
-    });
-  
-    const data = await response.json();
-    const messages = data.body;
+  const [messages, setMessages] = useState<any[]>([]);
+
+  useEffect(() => {
+    async function fetchMessages() {
+      try {
+        const response = await fetch('http://localhost:8080/messages', {
+          method: 'GET'
+        });
+
+        const data = await response.json()
+        console.log(data)
+        setMessages(data.body);
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchMessages();
+  }, [formState])
 
   return (
-    <div className="flex flex-col justify-center bg-white mt-5 rounded-xl min-h-40 drop-shadow-md">
-      {
-        messages ? <Messages messages={messages}/> : <NoMessage/>
-      }
-    </div>
+  <div className="flex flex-col justify-center bg-white mt-5 rounded-xl min-h-40 drop-shadow-md">
+    {
+      messages.length ? <Messages messages={messages}/> : <NoMessage/>
+    }
+  </div>
   )
 }
