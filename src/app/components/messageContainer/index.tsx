7562e8586d9e1d73d6c017c8543bc5dbc9b3ae8b
DavidSheltonSF <david.faria012@gmail.com>
2025-09-27 11:20:24 -0300

    feat(style): add loading animation when loading messages

--- src/app/components/messageContainer/index.tsx
+++ src/app/components/messageContainer/index.tsx
@@ -7,6 +7,7 @@ import { useEffect, useState } from "react";
 export function MessageContainer({formState}: {formState: any}) {
 
   const [messages, setMessages] = useState<any[]>([]);
+  const [loading, setLoading] = useState<any>(true);
 
   useEffect(() => {
     async function fetchMessages() {
@@ -20,13 +21,21 @@ export function MessageContainer({formState}: {formState: any}) {
         setMessages(data.body);
       } catch (error) {
         console.log(error)
+      } finally {
+        setLoading(false)
       }
     }
     fetchMessages();
   }, [formState])
 
+  {if (loading) return (
+    <div className="flex flex-col items-center justify-center bg-white mt-5 rounded-xl min-h-40 drop-shadow-md">
+      <img className="w-15 animate-spin" src="loading-icon.svg" alt="" />
+    </div>
+  )}
+
   return (
-  <div className="flex flex-col justify-center bg-white mt-5 rounded-xl min-h-40 drop-shadow-md">
+  <div className="flex flex-col items-center justify-center bg-white mt-5 rounded-xl min-h-40 drop-shadow-md">
     {
       messages.length ? <Messages messages={messages}/> : <NoMessage/>
     }
