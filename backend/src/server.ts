dcebce72758ae3423ad2b39c945051b4a2333029
DavidSheltonSF <david.faria012@gmail.com>
2025-09-26 22:50:31 -0300

    refactor(server):  integrate Repositories, usecases, controllers and the adapter

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,16 @@
-import express, {type Response, type Request} from 'express';
+import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import { FindAllMessagesController } from './controllers/FindAllMessagesController';
 import { CreateMessageController } from './controllers/CreateMessageController';
+import { MessageMongodbRepository } from './repositories/MessageMongodbRepository';
+import { FindAllMessagesUseCase } from './usecases/FindAllMessagesUseCase/FindAllMessagesUseCase';
+import { CreateMessageUseCase } from './usecases/CreateMessageUseCase/CreateMessageUseCase';
+import { MongodbHelper } from './repositories/helpers/MongodbHelper';
+import dotenv from 'dotenv';
+import { adaptExpress } from './adapter/adaptExpress';
+
+dotenv.config()
 
 const app = express()
 const port = 8080
@@ -11,15 +19,28 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors())
 
-const findAllMessagesController = new FindAllMessagesController();
-const createMessageController = new CreateMessageController();
+await (async () => {
+  const MONGO_URI = process.env.MONGO_URI
+  if(!MONGO_URI){
+    throw Error('Mongodb URI was not found')
+  }
+
+  const mongoHelper = MongodbHelper.getInstance()
+  await mongoHelper.connect(MONGO_URI);
+
+  const messageRepository = new MessageMongodbRepository()
+
+  const findAllMessagesUseCase = new FindAllMessagesUseCase(messageRepository)
+  const findAllMessagesController = new FindAllMessagesController(findAllMessagesUseCase);
+
+  const createMessageUseCase = new CreateMessageUseCase(messageRepository)
+  const createMessageController = new CreateMessageController(createMessageUseCase);
 
 
-app.get('/messages', findAllMessagesController.handle);
+  app.get('/messages', adaptExpress(findAllMessagesController));
+  app.post('/messages', adaptExpress(createMessageController));
 
-
-app.post('/messages', createMessageController.handle);
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-})
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  })
+})()
