ef6b22e8b2a81599051f0713ab113ddaf90dda01
DavidSheltonSF <david.faria012@gmail.com>
2025-09-27 08:59:41 -0300

    feat(MessageItem): use setTimeFeedBack to set a feedback message for each message acording to the date they ware created
    refactor(MessageItem): improve message style

--- src/app/components/messageItem/index.tsx
+++ src/app/components/messageItem/index.tsx
@@ -1,14 +1,16 @@
+import { setTimeFeedback } from "@/app/utils/setTimeFeedback";
+
 export interface MessageProps {
   id?: string;
   username: string;
   email: string;
   text: string;
-  // createdAt: string
+  createdAt: string
 }
 
 export function MessageItem(props: MessageProps) {
 
-  const {username, email, text} = props
+  const {username, email, text, createdAt} = props
  
   return (
     <article className="flex flex-col gap-4 h-30">
@@ -19,12 +21,13 @@ export function MessageItem(props: MessageProps) {
       <div className="flex flex-col justify-center mr-auto">
         <h3 className="font-bold">{username}</h3>
         <div className="flex items center gap-1">
-          <img className="opacity-50" src="email-icon.svg" alt="" /> <span className="text-gray-500">{email}</span>
+          <img className="opacity-50 w-4" src="email-icon.svg" alt="" /> <span className="text-gray-500 text-sm">{email}</span>
         </div>
       </div>
       <div>
           <div className="flex gap-1">
-          <img className="opacity-25" src="clock-icon.svg" alt="" /> <span className="text-gray-400">less than a minute ago</span>
+          <img className="opacity-25 w-4" src="clock-icon.svg" alt="" /> 
+          <span className="text-gray-400 text-sm">{setTimeFeedback(createdAt)}</span>
         </div>
       </div>
     </header>
