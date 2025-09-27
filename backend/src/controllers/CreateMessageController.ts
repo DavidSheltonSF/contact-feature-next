f761f362e5c39706639b5237a39227e678d633fb
DavidSheltonSF <david.faria012@gmail.com>
2025-09-26 22:48:55 -0300

    refactor(controllers): remove all express depencencies from controllers

--- backend/src/controllers/CreateMessageController.ts
+++ backend/src/controllers/CreateMessageController.ts
@@ -1,47 +1,56 @@
-import {type Request, type Response} from 'express'
+import { InvalidUsernameError } from '../usecases/errors/InvalidUsernameError';
+import { InvalidEmailError } from '../usecases/errors/InvalidEmailError';
+import { CreateMessage } from '../usecases/CreateMessageUseCase/interface';
+import { HttpRequest, HttpResponse } from './httpPort';
+import { badRequest, ok, serverError } from './helpers/httpHelpers';
 export class CreateMessageController {
 
-  async handle(req: Request, res: Response) {
+  private readonly useCase: CreateMessage
+
+  constructor(useCase: CreateMessage){
+    this.useCase = useCase
+  }
+
+  async handle(request: HttpRequest): Promise<HttpResponse> {
 
     try {
-      const body = req.body
+      const body = request.body
+      console.log(body)
 
       if (!body){
-        return res.status(400).send({
-          message: 'Request body is missing'
-        })
+        return badRequest('Request body is missing')
       }
 
-      const {username, email, message} = body;
+      const {username, email, text} = body;
 
-      
       if (!username){
-        return res.status(400).send({
-          message: 'Username is missing'
-        })
+        return badRequest('Username is missing')
       }
 
       if (!email){
-        return res.status(400).send({
-          message: 'User email is required'
-        })
+        return badRequest('User email is required')
       }
 
-      if (!message){
-        return res.status(400).send({
-          message: 'User message is required'
-        })
+      if (!text){
+        return badRequest('User text message is required')
       }
 
-      return res.status(200).send({
-        message: 'Message registred successfuly'
-      })
+      const result = await this.useCase.execute(body)
+
+      return ok(result)
 
     } catch (error) {
       console.log(error);
-      return res.status(500).send({
-        message: 'Internal server error'
-      })
+
+      if(error instanceof InvalidUsernameError) {
+        return badRequest(error.message)
+      }
+
+      if(error instanceof InvalidEmailError) {
+        return badRequest(error.message)
+      }
+
+      return serverError('Internal server error')
     }
   }
 }
--- backend/src/controllers/FindAllMessagesController.ts
+++ backend/src/controllers/FindAllMessagesController.ts
@@ -1,19 +1,32 @@
 import {type Request, type Response} from 'express'
 import { fakeMessages } from '../mocks/messageMocks';
+import { FindAllMessages } from '../usecases/FindAllMessagesUseCase/interface';
+import { HttpRequest } from './httpPort';
 
 export class FindAllMessagesController {
 
-  async handle(req: Request, res: Response) {
+  private readonly useCase: FindAllMessages
+  
+  constructor(useCase: FindAllMessages){
+    this.useCase = useCase
+  }
 
-    const sortedMessages = fakeMessages.sort((a, b) => {
+  async handle(request: HttpRequest) {
+
+    const results = await this.useCase.execute();
+
+    const sortedMessages = results.sort((a, b) => {
       const aCreatedAt = new Date(a.createdAt)
       const bCreatedAt = new Date(b.createdAt)
       return bCreatedAt.getTime() - aCreatedAt.getTime()
     });
 
-    return res.send({
-      data: sortedMessages
-    })
+    return {
+      statusCode: 200,
+      body: sortedMessages
+    }
   }
 
 }
+
+
