ed017ab4a91501ee00f35c0d49f002fa79ac584e
DavidSheltonSF <david.faria012@gmail.com>
2025-09-29 15:37:20 -0300

    refactor(navbar): enhance design

--- src/app/components/navbar/index.tsx
+++ src/app/components/navbar/index.tsx
@@ -1,11 +1,11 @@
 export function NavBar() {
   return (
     <header>
-      <nav className="flex p-5 bg-gray-50 drop-shadow-sm">
+      <nav className="flex items-center justify-center p-2 bg-gray-50 drop-shadow-sm">
         <div className="mr-auto">
           <img className="rounded-md" src="websiteIcon.png" alt="" />
         </div>
-        <span className="bg-green-200 py-1 px-2 rounded-2xl font-bold">
+        <span className="flex items-center h-10 bg-green-200 px-2 rounded-2xl font-bold">
           Live Demo
         </span>
       </nav>
