9406015e4b75f8aab87a01894141cfec87eb59cb
DavidSheltonSF <david.faria012@gmail.com>
2025-09-29 15:55:15 -0300

    fix(Nomessage component): make it fill all his parent's remaining height

--- src/app/components/noMessage/index.tsx
+++ src/app/components/noMessage/index.tsx
@@ -1,6 +1,6 @@
 export function NoMessage() {
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="flex flex-col flex-1 items-center justify-center">
       <h2 className="font-bold">No messages yet</h2>
       <p>Be the first to send us a message</p>
     </div>
