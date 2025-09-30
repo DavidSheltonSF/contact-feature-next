79ae2ed83187fb8985bc8d7d5b02ac9d63bf5081
DavidSheltonSF <david.faria012@gmail.com>
2025-09-29 22:40:29 -0300

    refactor(Messages): make cards grow verticaly to contain a big text inside

--- src/app/components/messageItem/index.tsx
+++ src/app/components/messageItem/index.tsx
@@ -13,29 +13,29 @@ export function MessageItem(props: MessageProps) {
   const {fullName, email, text, createdAt} = props
  
   return (
-    <article className="flex flex-col gap-4 h-30 transform opacity-0 transition-all translate-y-4 ease-in-out delay-200 duration-200 animate-fade-in">
-    <header className="flex gap-3">
-      <div className="flex">
-        <span className="bg-blue-400 px-3 py-3 rounded-xl">
-          <img src="user-icon.svg" alt="" />
-        </span>
-      </div>
-      <div className="flex flex-col justify-center mr-auto">
-        <h3 className="font-bold">{fullName}</h3>
-        <div className="flex items center gap-1">
-          <img className="opacity-50 w-4" src="email-icon.svg" alt="" /> <span className="text-gray-500 text-sm">{email}</span>
+    <article className="flex flex-col gap-4 h-auto px-5 transform opacity-0 transition-all translate-y-4 ease-in-out delay-200 duration-200 animate-fade-in">
+      <header className="flex gap-3">
+        <div className="flex">
+          <span className="bg-blue-400 px-3 py-3 rounded-xl">
+            <img src="user-icon.svg" alt="" />
+          </span>
         </div>
-      </div>
-      <div>
-          <div className="flex gap-1">
-          <img className="opacity-25 w-4" src="clock-icon.svg" alt="" /> 
-          <span className="text-gray-400 text-sm">{setTimeFeedback(createdAt)}</span>
+        <div className="flex flex-col justify-center mr-auto">
+          <h3 className="font-bold">{fullName}</h3>
+          <div className="flex items center gap-1">
+            <img className="opacity-50 w-4" src="email-icon.svg" alt="" /> <span className="text-gray-500 text-sm">{email}</span>
+          </div>
         </div>
-      </div>
-    </header>
-    <main>
-      <p>{text}</p>
-    </main>
-  </article>
+        <div>
+            <div className="flex gap-1">
+            <img className="opacity-25 w-4" src="clock-icon.svg" alt="" /> 
+            <span className="text-gray-400 text-sm">{setTimeFeedback(createdAt)}</span>
+          </div>
+        </div>
+      </header>
+      <main>
+        <p>{text}</p>
+      </main>
+    </article>
   )
 }
